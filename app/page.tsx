c61711cdf0dd4794b2000e8d5301bf6796b85bea
shahnoorgit <shahnoormujawar@gmail.com>
2024-06-08 15:38:49 +0530

    block 1 done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
-import { CiCirclePlus } from "react-icons/ci";
+import { CiCirclePlus, CiImageOn } from "react-icons/ci";
+import { IoVideocamOutline } from "react-icons/io5";
+import { TbMusicStar } from "react-icons/tb";
 import { VscBellDot } from "react-icons/vsc";
 
 export default function Home() {
@@ -18,7 +20,7 @@ export default function Home() {
   return (
     <main className="bg-[#d4d4d4] min-h-screen min-w-screen flex justify-center items-center">
       <div className="w-[95vw] h-[95vh] shadow-2xl bg-white flex flex-col">
-        <div className="w-full h-1/2 overflow-hidden bg-[#265c4c] md:p-6">
+        <div className="w-full h-1/2 overflow-hidden bg-[#265c4c] md:p-4">
           <div className="w-full h-full overflow-hidden bg-[#265c4c]">
             <div className="w-full min-h-10 flex items-center justify-between">
               <div className="flex-1 flex gap-2 justify-start items-center">
@@ -63,15 +65,161 @@ export default function Home() {
                   </span>
                 ))}
               </div>
-              <div className="w-1/3 h-full bg-[#3c856f]">Div 2</div>
-              <div className="w-1/3 h-full bg-[#3c856f]">Div 3</div>
-              <div className="w-1/3 h-full bg-[#3c856f]">Div 4</div>
-              <div className="w-1/6 h-full bg-[#3c856f]">Div 5</div>
+              <div className="w-1/3 h-full bg-[#3c856f]">
+                <div className="  w-full h-1/5">
+                  <div className=" ml-3 mt-3 h-12 w-12 shadow-2xl  bg-[rgb(38,92,76)] rounded-full flex justify-center items-center">
+                    <CiImageOn className=" text-2xl text-yellow-300" />
+                  </div>
+                </div>
+                <div className=" ml-3 w-full h-1/4">
+                  <div className=" flex flex-col gap-1   justify-start">
+                    <span className=" text-gray-300 font-normal">Pictures</span>
+                    <span className=" text-white font-bold text-5xl">6455</span>
+                  </div>
+                </div>
+                <div className="border-t p-5 flex gap-2 justify-center items-center border-green-800 w-full h-1/3">
+                  <div className=" h-full w-1/3 border-green-800  border-r">
+                    <div className=" flex flex-col justify-center">
+                      <span className=" font-medium text-gray-300">Total</span>
+                      <span className=" text-white font-semibold text-2xl">
+                        100GB
+                      </span>
+                    </div>
+                  </div>
+                  <div className=" h-full w-1/3 border-green-800  border-r">
+                    <div className=" flex flex-col justify-center">
+                      <span className=" font-medium text-gray-300">Used</span>
+                      <span className=" text-white font-semibold text-2xl">
+                        43GB
+                      </span>
+                    </div>
+                  </div>
+                  <div className=" h-full w-1/3">
+                    <div className=" flex flex-col justify-center">
+                      <span className=" font-medium text-gray-300">
+                        Available
+                      </span>
+                      <span className=" text-white font-semibold text-2xl">
+                        57GB
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="w-1/3 h-full bg-[#3c856f]">
+                <div className="  w-full h-1/5">
+                  <div className=" ml-3 mt-3 h-12 w-12 shadow-2xl  bg-[#265c4c] rounded-full flex justify-center items-center">
+                    <IoVideocamOutline className=" text-2xl text-yellow-300" />
+                  </div>
+                </div>
+                <div className=" ml-3 w-full h-1/4">
+                  <div className=" flex flex-col gap-1   justify-start">
+                    <span className=" text-gray-300 font-normal">Videos</span>
+                    <span className=" text-white font-bold text-5xl">3221</span>
+                  </div>
+                </div>
+                <div className="border-t p-5 flex gap-2 justify-center items-center border-green-800 w-full h-1/3">
+                  <div className=" h-full w-1/3 border-green-800  border-r">
+                    <div className=" flex flex-col justify-center">
+                      <span className=" font-medium text-gray-300">Total</span>
+                      <span className=" text-white font-semibold text-2xl">
+                        100GB
+                      </span>
+                    </div>
+                  </div>
+                  <div className=" h-full w-1/3 border-green-800  border-r">
+                    <div className=" flex flex-col justify-center">
+                      <span className=" font-medium text-gray-300">Used</span>
+                      <span className=" text-white font-semibold text-2xl">
+                        43GB
+                      </span>
+                    </div>
+                  </div>
+                  <div className=" h-full w-1/3">
+                    <div className=" flex flex-col justify-center">
+                      <span className=" font-medium text-gray-300">
+                        Available
+                      </span>
+                      <span className=" text-white font-semibold text-2xl">
+                        57GB
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="w-1/3 h-full bg-[#3c856f]">
+                <div className="  w-full h-1/5">
+                  <div className=" ml-3 mt-3 h-12 w-12 shadow-2xl  bg-[#265c4c] rounded-full flex justify-center items-center">
+                    <TbMusicStar className=" text-2xl text-yellow-300" />
+                  </div>
+                </div>
+                <div className=" ml-3 w-full h-1/4">
+                  <div className=" flex flex-col gap-1   justify-start">
+                    <span className=" text-gray-300 font-normal">Sounds</span>
+                    <span className=" text-white font-bold text-5xl">1443</span>
+                  </div>
+                </div>
+                <div className="border-t p-5 flex gap-2 justify-center items-center border-green-800 w-full h-1/3">
+                  <div className=" h-full w-1/3 border-green-800  border-r">
+                    <div className=" flex flex-col justify-center">
+                      <span className=" font-medium text-gray-300">Total</span>
+                      <span className=" text-white font-semibold text-2xl">
+                        100GB
+                      </span>
+                    </div>
+                  </div>
+                  <div className=" h-full w-1/3 border-green-800  border-r">
+                    <div className=" flex flex-col justify-center">
+                      <span className=" font-medium text-gray-300">Used</span>
+                      <span className=" text-white font-semibold text-2xl">
+                        43GB
+                      </span>
+                    </div>
+                  </div>
+                  <div className=" h-full w-1/3">
+                    <div className=" flex flex-col justify-center">
+                      <span className=" font-medium text-gray-300">
+                        Available
+                      </span>
+                      <span className=" text-white font-semibold text-2xl">
+                        57GB
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="w-1/6 h-full bg-[rgb(60,133,111)]">
+                <div className=" flex flex-col h-full gap-1">
+                  <div className="flex mt-10 flex-col justify-center items-center h-1/4">
+                    <span className="font-medium text-gray-300">
+                      Used Space
+                    </span>
+                    <span className=" font-semibold text-5xl text-yellow-300">
+                      64.2<span className=" text-sm absolute">%</span>
+                    </span>
+                  </div>
+                  <div className=" h-full w-full flex flex-col">
+                    <div className="w-full h-[15%] diagonal-lines"></div>
+                    <div className="w-full h-[40%] diagonal-lines_B"></div>
+                    <div className="w-full h-[40%] bg-black diagonal-lines"></div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <div className="w-full h-1/2 overflow-hidden bg-white md:p-6">
-          <div className="w-full h-full overflow-hidden bg-white">box-2</div>
+          <div className="w-full flex gap-10 h-full overflow-hidden bg-white">
+            <div className="h-full flex flex-col gap-5 w-1/3">
+              <div className=" w-full flex justify-between border-black h-[15%] border-b">
+                <span className=" font-bold text-2xl">Team Activity</span>
+                <div className=" flex gap-3 justify-center items-center">
+                  hi
+                </div>
+              </div>
+            </div>
+            <div className="h-full w-2/3 bg-slate-100">2</div>
+          </div>
         </div>
       </div>
     </main>
